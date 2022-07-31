5dc8a63c25c72be5771e9ba6bac8cba1f3670e80
TheBigR <nevoroy@gmail.com>
2022-07-31 17:47:19 +0300

    changed to a proper name

--- src/services/fakeNames.js
+++ src/services/fakeNames.js
@@ -54,8 +54,8 @@ module.exports = {
       indexUpdater(firstName)
       indexUpdater(lastName)
     }
-    const shit = highestFinder(indexObj, 10, initTime)
+    const data = highestFinder(indexObj, 10, initTime)
 
-    return shit
+    return data
   },
 }
